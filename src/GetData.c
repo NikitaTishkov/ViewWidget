23d823659ebdadadad7f227b2a9e83159852967b
NikitaTishkov <nikitatishkov77@gmail.com>
2020-07-08 00:21:14 +0300

    Fix concatenation error

--- src/GetData.c
+++ src/GetData.c
@@ -6,29 +6,22 @@ const char* api_key = "AIzaSyBgxTSi0WNEyNNN7JyuimLMgk9XMGT2bjI";
 void GetFromApi(char* id){
     FILE* data = fopen("temp.json", "w");
     CURL *curl = curl_easy_init();
-    /*char* url = "https://www.googleapis.com/youtube/v3/videos?id=" + id 
-                                                            + "&key="
-                                                            + api_key
-                                                            + "&part="
-                                                            + "statistics";
-                                                            */
-    char url[] = "https://www.googleapis.com/youtube/v3/videos?id=";
-    strcat(url, id);
-    strcat(url,"&key=");
-    strcat(url, api_key);
-    strcat(url, "&part=");
-    strcat(url, "statistics");
+    
+    char url[256];
+    sprintf(url, "https://www.googleapis.com/youtube/v3/videos?id=%s&key=%s&part=statistics", id, api_key);
+
+    
     CURLcode res1 = curl_easy_setopt(curl, CURLOPT_URL, url);
-    printf("1: %s \n", curl_easy_strerror(res1));
+    //printf("1: %s \n", curl_easy_strerror(res1));
 
     CURLcode res2 = curl_easy_setopt(curl, CURLOPT_HTTPGET, 0);
-    printf("2: %s \n", curl_easy_strerror(res2));
+    //printf("2: %s \n", curl_easy_strerror(res2));
     CURLcode res3 = curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
-    printf("3: %s \n", curl_easy_strerror(res3));
+    //printf("3: %s \n", curl_easy_strerror(res3));
     CURLcode res4 = curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
-    printf("4: %s \n", curl_easy_strerror(res4));
+    //printf("4: %s \n", curl_easy_strerror(res4));
     CURLcode res5 = curl_easy_perform(curl);
-    printf("5: %s \n", curl_easy_strerror(res5));
+    //printf("5: %s \n", curl_easy_strerror(res5));
     curl_easy_cleanup(curl);
 
     fclose(data);
